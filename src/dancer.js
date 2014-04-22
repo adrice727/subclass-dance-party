a976a2237e7606b296413ca11886ade1396f0a31
Hack Reactor Students <adrice727@gmail.com>
2014-04-22 16:44:06 -0700

    Condensed dancer files into one file.  Need to fix blinky dancer.

--- src/dancer.js
+++ src/dancer.js
@@ -1,47 +1,115 @@
-// Creates and returns a new dancer object that can step
+// Dancer superclass
 var Dancer = function(top, left, timeBetweenSteps){
   this._timeBetweenSteps = timeBetweenSteps;
   this._top = top;
   this._left = left;
-  // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
   this.setPosition(top, left);
   this._linedup = false;
   this._nearOtherDancer = false;
 };
-
 Dancer.prototype.step = function(){
-  // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
   var that = this;
   setTimeout(function(){
     that.step();
   }, that._timeBetweenSteps);
-};
 
+  if ( that._nearOtherDancer ) {
+    that.$node.find('img').attr('src', 'images/kitty.gif');
+  }
+};
 Dancer.prototype.setPosition = function(top, left){
-  // Use css top and left properties to position our <span> tag
-  // where it belongs on the page. See http://api.jquery.com/css/
-  //
   this._top = top;
   this._left = left;
-
   this.$node.css({
     top: top,
     left: left
   });
 };
-
 Dancer.prototype.lineUp = function(){
-
-  var randomheight = $("body").height() * Math.random();
+  var randomheight = $(".canvas").height() * Math.random();
   this.setPosition(randomheight, 0);
   this._linedup = true;
-
 };
-
 Dancer.prototype.danceAgain = function(){
-
-  this.setPosition($("body").height() * Math.random(), $("body").width() * Math.random());
+  this.setPosition($(".canvas").height() * Math.random(), $("body").width() * Math.random());
 };
+
+// BlinkyDancer
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+};
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+BlinkyDancer.prototype.oldStep =  Dancer.prototype.step;
+BlinkyDancer.prototype.step = function(){
+  this.oldStep();
+  this.$node.toggle();
+};
+var makeBlinkyDancer = function(top, left, timeBetweenSteps){
+  var result = new BlinkyDancer(top, left, timeBetweenSteps);
+  return result;
+};
+
+// SpinningDancer
+var SpinningDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.prepend('<img class="spinner" src="images/homer.gif"/>');
+  this.$node.addClass('spinning');//.removeClass('dancer');
+  // this.$node.addClass('spinning');
+};
+
+SpinningDancer.prototype = Object.create(Dancer.prototype);
+SpinningDancer.prototype.constructor = SpinningDancer;
+SpinningDancer.prototype.oldStep =  Dancer.prototype.step;
+SpinningDancer.prototype.step = function(){
+  this.oldStep();
+};
+
+var makeSpinningDancer = function(top, left, timeBetweenSteps){
+  var result = new SpinningDancer(top, left, timeBetweenSteps);
+  return result;
+};
+
+// FlyingDancer
+var FlyingDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.prepend('<img class="flyer" src="images/astronaut.png" />');
+  this.$node.addClass('flyer')//.removeClass('dancer');
+  this.large = false;
+};
+FlyingDancer.prototype = Object.create(Dancer.prototype);
+FlyingDancer.prototype.constructor = FlyingDancer;
+FlyingDancer.prototype.oldStep =  Dancer.prototype.step;
+FlyingDancer.prototype.step = function(){
+  this.oldStep();
+};
+var makeFlyingDancer = function(top, left, timeBetweenSteps){
+  var result = new FlyingDancer(top, left, timeBetweenSteps);
+  return result;
+};
+
+//Traveling Dancer
+var TravelingDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.prepend('<img class="spinner" src="images/elephant.png"/>');
+  // this.$node.addClass('traveling').removeClass('dancer');
+};
+TravelingDancer.prototype = Object.create(Dancer.prototype);
+TravelingDancer.prototype.constructor = TravelingDancer;
+TravelingDancer.prototype.oldStep =  Dancer.prototype.step;
+TravelingDancer.prototype.step = function(){
+  this.oldStep();
+  if ( !this._linedup){
+    var randomHeight = $(".canvas").height() * Math.random();
+    var randomWidth = $(".canvas").height() * Math.random();
+    this.setPosition(randomHeight, randomWidth);
+  }
+
+};
+var makeTravelingDancer = function(top, left, timeBetweenSteps){
+  var result = new TravelingDancer(top, left, timeBetweenSteps);
+  return result;
+};
+
